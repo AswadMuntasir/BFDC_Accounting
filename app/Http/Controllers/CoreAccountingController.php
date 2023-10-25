a7d9151ab2536fee2e1d8173e3d479d1b5ab494a
Aswad Muntasir <aswadmuntasir04@gmail.com>
2023-10-25 12:36:07 +0600

    Update CoreAccountingController.php

--- app/Http/Controllers/CoreAccountingController.php
+++ app/Http/Controllers/CoreAccountingController.php
@@ -1829,7 +1829,7 @@ class CoreAccountingController extends Controller
                 $ledgerData = DB::table('voucher_entry')
                     ->select('voucher_no', 'description', 'voucher_date', 'dr_amount', 'cr_amount')
                     ->whereBetween('voucher_date', ['2023-07-01', '2023-07-31'])
-                    ->whereIn('status', ['pending', 'Done'])
+                    ->whereIn('status', ['pending', 'Done', 'Pending'])
                     ->where(function ($query) use ($name) {
                         $query->where(function ($query) use ($name) {
                             $query->where('voucher_type', 'Journal')
